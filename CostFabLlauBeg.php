e6d2c256b46c28fc3bacce021ec1d2d37baa5b39
dacomo dacomo <dcolla2@proton.me>
2023-11-13 09:52:23 +0100

    Cost Fabricació de llaunes de begudes - GP1F4A8

--- CostFabLlauBeg.php
+++ CostFabLlauBeg.php
@@ -38,8 +38,25 @@
 			return $mostra_cost;			
  		}	
 	}
-	$llauna_beguda = new llauna($_GET['r'],$_GET['l'],$_GET['g'],$_GET['d'],$_GET['cpg']);
-	echo $llauna_beguda->mostra_cost_fabricacio_llauna_en_dolars();
 ?>
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			Cost de Fabricació de llauna de begudes
+		</title>
+	</head>
+	<body>
+		<?php
+			$llauna_beguda = new llauna($_GET['r'],$_GET['l'],$_GET['g'],$_GET['d'],$_GET['cpg']);
+			echo $llauna_beguda->mostra_cost_fabricacio_llauna_en_dolars();
+			echo '<p><font size="1">Servidor: '.getenv('HOST_NAME').'</font></p>';
+		?>
+		<a href="index.php">Torna al formulari inicial</a>
+	</body>
+</html>
+
+
 	
 	
